42bbac1bd9b0197ce892c18ce4bf4468dd2a1599
zzx2002 <88384251+zzx6869@users.noreply.github.com>
2021-08-03 21:31:26 +0800

    Add files via upload

---
+++ build-sudoku-Desktop_Qt_5_14_2_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
@@ -0,0 +1,169 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.14.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../sudoku2/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.14.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[17];
+    char stringdata0[232];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 15), // "on_exit_clicked"
+QT_MOC_LITERAL(2, 27, 0), // ""
+QT_MOC_LITERAL(3, 28, 18), // "on_newgame_clicked"
+QT_MOC_LITERAL(4, 47, 17), // "show_prodcut_sets"
+QT_MOC_LITERAL(5, 65, 3), // "row"
+QT_MOC_LITERAL(6, 69, 3), // "col"
+QT_MOC_LITERAL(7, 73, 10), // "change_num"
+QT_MOC_LITERAL(8, 84, 1), // "s"
+QT_MOC_LITERAL(9, 86, 18), // "on_confirm_clicked"
+QT_MOC_LITERAL(10, 105, 17), // "on_answer_clicked"
+QT_MOC_LITERAL(11, 123, 14), // "on_tip_clicked"
+QT_MOC_LITERAL(12, 138, 19), // "on_recall_2_clicked"
+QT_MOC_LITERAL(13, 158, 15), // "on_save_clicked"
+QT_MOC_LITERAL(14, 174, 16), // "on_clear_clicked"
+QT_MOC_LITERAL(15, 191, 16), // "on_solve_clicked"
+QT_MOC_LITERAL(16, 208, 23) // "on_user_defined_clicked"
+
+    },
+    "MainWindow\0on_exit_clicked\0\0"
+    "on_newgame_clicked\0show_prodcut_sets\0"
+    "row\0col\0change_num\0s\0on_confirm_clicked\0"
+    "on_answer_clicked\0on_tip_clicked\0"
+    "on_recall_2_clicked\0on_save_clicked\0"
+    "on_clear_clicked\0on_solve_clicked\0"
+    "on_user_defined_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      12,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   74,    2, 0x08 /* Private */,
+       3,    0,   75,    2, 0x08 /* Private */,
+       4,    2,   76,    2, 0x08 /* Private */,
+       7,    3,   81,    2, 0x08 /* Private */,
+       9,    0,   88,    2, 0x08 /* Private */,
+      10,    0,   89,    2, 0x08 /* Private */,
+      11,    0,   90,    2, 0x08 /* Private */,
+      12,    0,   91,    2, 0x08 /* Private */,
+      13,    0,   92,    2, 0x08 /* Private */,
+      14,    0,   93,    2, 0x08 /* Private */,
+      15,    0,   94,    2, 0x08 /* Private */,
+      16,    0,   95,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int, QMetaType::Int,    5,    6,
+    QMetaType::Void, QMetaType::QString, QMetaType::Int, QMetaType::Int,    8,    5,    6,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_exit_clicked(); break;
+        case 1: _t->on_newgame_clicked(); break;
+        case 2: _t->show_prodcut_sets((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
+        case 4: _t->on_confirm_clicked(); break;
+        case 5: _t->on_answer_clicked(); break;
+        case 6: _t->on_tip_clicked(); break;
+        case 7: _t->on_recall_2_clicked(); break;
+        case 8: _t->on_save_clicked(); break;
+        case 9: _t->on_clear_clicked(); break;
+        case 11: _t->on_user_defined_clicked(); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_MainWindow.data,
+    qt_meta_data_MainWindow,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 12)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 12;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 12)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 12;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
